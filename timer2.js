cba2aebb19d2cf19152ba2c5e59d12253a8f7549
William <mack.mindenhall@gmail.com>
2020-06-30 13:34:41 +0000

    Noise occurs as soon as 'b' is pressed

--- timer2.js
+++ timer2.js
@@ -1,26 +1,11 @@
-// const readline = require("readline");
 const stdin = process.stdin;
 
 stdin.setRawMode(true);
 stdin.setEncoding("utf8");
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-// rl.line(())
-// stdin.on("keypress", (str, key) => {
-//   if (key.ctrl && key.name === "c") {
-//     process.exit();
-//   } else {
-//     console.log("key: ", key);
-//     console.log("str: ", str);
-//   }
-// });
 
 stdin.on("data", (key) => {
   if (key === "b") {
-    console.log("you pressed b");
+    process.stdout.write("\x07");
   }
   if (key.match(/[1-9]/) !== null) {
     console.log(`setting timer for ${key * 1000} seconds...`);
